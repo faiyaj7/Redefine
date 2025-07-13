44f360fcc2e3e24fdb94db9c5c21353bd5648890
faiyaj7 <faiyaj.bin.ahmed@gmail.com>
2025-07-14 00:09:57 +0600

    About Section Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
+import About from "./components/About";
 import Hero from "./components/Hero";
-import gsap, { ScrollTrigger } from "gsap/all";
-gsap.registerPlugin(ScrollTrigger);
+import gsap, { ScrollTrigger, SplitText } from "gsap/all";
+gsap.registerPlugin(ScrollTrigger , SplitText);
 function App() {
   return (
     <main className="min-h-screen w-screen relative overflow-x-hidden">
       <Hero />
-      {/* <div className="bg-amber-400 opacity-0 hover:opacity-100 text-black">hello</div> */}
-      <div className="h-screen"></div>
+      <About />
     </main>
   );
 }
